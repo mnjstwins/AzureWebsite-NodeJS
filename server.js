76466ffbc5fa7faba866661495c7f2901bf17220
rtinsley <roger.tinsley@asidua.com>
2015-01-16 13:27:09 +0000

    Changes

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ var http = require('http')
 var port = process.env.PORT || 80;
 http.createServer(function(req, res) {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello World from a rainy Belfast\n');
+  res.end('Hello World!!!!\n');
 }).listen(port);
